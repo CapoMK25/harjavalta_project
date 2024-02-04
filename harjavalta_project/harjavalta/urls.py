9cdbf72fd8a03b30186c50ef497416ef76f48d09
MK <parisburner6969@gmail.com>
2024-02-04 21:45:22 +0200

    Changed the Django framework to further complement the project

--- harjavalta_project/harjavalta/urls.py
+++ harjavalta_project/harjavalta/urls.py
@@ -1,6 +1,6 @@
 from django.urls import path
-from .views import main
+from .views import index
 
 urlpatterns = [
-    path('', main)
+    path('', index, name='index'),
 ]
--- harjavalta_project/harjavalta_project/settings.py
+++ harjavalta_project/harjavalta_project/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/5.0/ref/settings/
 """
 
 from pathlib import Path
+import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -37,8 +38,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'harjavalta.apps.HarjavaltaConfig',
-    'rest_framework'
+    'harjavalta'
 ]
 
 MIDDLEWARE = [
@@ -118,6 +118,10 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.0/howto/static-files/
 
 STATIC_URL = 'static/'
+
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, 'harjavalta/static'),
+]
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
