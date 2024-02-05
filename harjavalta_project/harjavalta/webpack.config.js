c40bb5f0cabd61a7727069d0ce3b4da00330a8af
MK <parisburner6969@gmail.com>
2024-02-05 14:30:25 +0200

    Modified React to a working state within the project

---
+++ harjavalta_project/harjavalta/webpack.config.js
@@ -0,0 +1,25 @@
+const path = require("path");
+
+module.exports = {
+  mode: 'development',
+  entry: path.resolve(__dirname, "./src/index.js"),
+  output: {
+    path: path.resolve(__dirname, "static/harjavalta"),
+    filename: "bundle.js",
+  },
+  resolve: {
+    modules: [path.resolve(__dirname, "harjavalta/src"), "node_modules"],
+    extensions: [".js", ".json", ".jsx"],
+  },
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        use: {
+          loader: "babel-loader",
+        },
+      },
+    ],
+  },
+};
