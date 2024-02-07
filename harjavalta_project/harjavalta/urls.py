a2902bbecd237a2d17e5b6be3445f875c9fbdf61
MK <parisburner6969@gmail.com>
2024-02-07 03:45:10 +0200

    Added new sites and settings to the project

--- harjavalta_project/harjavalta/urls.py
+++ harjavalta_project/harjavalta/urls.py
@@ -3,4 +3,9 @@ from . import views
 
 urlpatterns = [
     path('', views.index, name='index'),
+    path('koti/', views.koti, name='koti'),
+    path('työpaikat/', views.työpaikat, name='työpaikat'),
+    path('yrityksille/', views.yrityksille, name='yrityksille'),
+    path('työntekijöille/', views.työntekijöille, name='työntekijöille'),
+    path('info/', views.info, name='info'),
 ]
--- harjavalta_project/harjavalta/views.py
+++ harjavalta_project/harjavalta/views.py
@@ -1,4 +1,21 @@
 from django.shortcuts import render
+from django.shortcuts import redirect
+from django.urls import reverse
 
 def index(request):
     return render(request, 'index.html')
+
+def koti(request):
+    return redirect('index')  # Redirect to the landing page
+
+def työpaikat(request):
+    return render(request, 'työpaikat.html')
+
+def yrityksille(request):
+    return render(request, 'yrityksille.html')
+
+def työntekijöille(request):
+    return render(request, 'työntekijöille.html')
+
+def info(request):
+    return render(request, 'info.html')
